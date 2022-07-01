ed2ff8e7407f8a74bfb1c4de87fe3c31d6dc3722
AJ Timek <Timek612@gmail.com>
2022-07-01 15:31:58 -0500

    done

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -90,31 +90,66 @@ console.log(find(10, [1,2,3,4,5,6,7,8]));
 
 
 
+
+
   
 // ----------------------
 // Stretch Goals
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
+// .charAt() is what we need. If 'letter' is equal to string.charAt(0), return true.
 function isFirstLetter(letter, string) {
-
+  if (letter === string.charAt(0)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+//Go through and add 0th index to sum and then loop through it and add 1st to sum and so on.
+function sumAll(array) {
   let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    (sum += array[i])
+  }
   // TODO: loop to add items
   return sum;
 }
+console.log('The sum of the array is:', sumAll([2, 200, 83, 40]));
+console.log('The sum of the array is:', sumAll([2, -200, -83, 40]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-
-
+function positiveOnly(array) {
+  let posOnly = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      posOnly.push(array[i])
+    }
+    
+  }
+  return posOnly;
+}
+console.log('The positive numbers in this array are:', positiveOnly([40, -50, 36, -65, 100, 0]));
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+// https://edabit.com/challenge/5L368ypvh5ZZ2HJSL
+// Create a function that takes an array of numbers arr and returns an inverted array.
+
+function invertArray(array) {
+  let arrayInverted = [];
+  for (let i = 0; i < array.length; i++) {
+    array[i] *= -1 
+    arrayInverted.push(array[i])
+  }
+  return arrayInverted;
+}
+console.log('Inverted array:', invertArray([1, -1, 2, -2]));
